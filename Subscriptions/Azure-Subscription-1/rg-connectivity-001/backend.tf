dcecb2c8989122452c2a0d6a8907bc687c318d93
CapDaveB <david.c.brown@capgemini.com>
2022-11-03 13:26:01 -0400

    Fixed

--- Subscriptions/Azure-Subscription-1/rg-connectivity-001/backend.tf
+++ Subscriptions/Azure-Subscription-1/rg-connectivity-001/backend.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name     = "rg-terraform-state-001"
     storage_account_name    = "storageaccountterraninj"
     container_name          = "tfstate"
-    key                     = "GitHub-Terraform-rg-loganalytics-001"
+    key                     = "GitHub-Terraform-rg-connectivity-001"
   }
 }
 provider "azurerm" {
